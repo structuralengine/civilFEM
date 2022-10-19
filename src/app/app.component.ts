465fbfe6de7947f9aa815c9e02b2e780bff4e535
sasaco <sasaicco@gmail.com>
2022-10-19 21:35:09 +0900

    angularバージョン復活

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,36 +1,11 @@
-import { ViewChild, ElementRef, Component } from '@angular/core';
-
-import vtkFullScreenRenderWindow from 'vtk.js/Sources/Rendering/Misc/FullScreenRenderWindow';
-import vtkConeSource from 'vtk.js/Sources/Filters/Sources/ConeSource';
-import vtkActor from 'vtk.js/Sources/Rendering/Core/Actor';
-import vtkMapper from 'vtk.js/Sources/Rendering/Core/Mapper';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-app';
 
-  @ViewChild('content', {read: ElementRef}) content: ElementRef;
 
-  fullscreenRenderWindow = null;
-
-  ngAfterViewInit(): void {
-    this.fullscreenRenderWindow = vtkFullScreenRenderWindow.newInstance();
-    const cone = vtkConeSource.newInstance();
-    const actor = vtkActor.newInstance();
-    const mapper = vtkMapper.newInstance();
-
-    actor.setMapper(mapper);
-    mapper.setInputConnection(cone.getOutputPort());
-
-    const renderer = this.fullscreenRenderWindow.getRenderer();
-    renderer.addActor(actor);
-    renderer.resetCamera();
-
-    const renderWindow = this.fullscreenRenderWindow.getRenderWindow();
-    renderWindow.render();
-  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,36 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { DragDropModule } from "@angular/cdk/drag-drop";
+import { FormsModule } from "@angular/forms";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
+import { CodemirrorModule } from '@ctrl/ngx-codemirror';
+
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ThreeComponent } from './components/three/three.component';
+import { CodeEditorComponent } from './components/code-editor/code-editor.component';
+import { SideLeftComponent } from './components/side-left/side-left.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ThreeComponent,
+
+    CodeEditorComponent,
+    SideLeftComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    DragDropModule,
+    BrowserAnimationsModule,
+
+    CodemirrorModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
